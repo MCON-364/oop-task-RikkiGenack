e19d319ae105d8bbd0b27e8c35a885ba46e5d29a
RikkiGenack <rgenack@student.touro.edu>
2026-02-17 01:00:07 -0500

    Update DemoMainTest.java

--- src/test/java/edu/touro/las/mcon364/taskmanager/DemoMainTest.java
+++ src/test/java/edu/touro/las/mcon364/taskmanager/DemoMainTest.java
@@ -131,9 +131,9 @@ class DemoMainTest {
 
 
         // It silently fails with a warning message
-       // assertDoesNotThrow(() -> {
-         //   testManager.run(new UpdateTaskCommand(testRegistry, "Non-existent task", Priority.HIGH));
-       // }, "Updating non-existent task should not throw (before custom exception refactoring)");
+        assertDoesNotThrow(() -> {
+            testManager.run(new UpdateTaskCommand(testRegistry, "Non-existent task", Priority.HIGH));
+        }, "Updating non-existent task should not throw (before custom exception refactoring)");
 
         // Verify task was not created
         assertNull(testRegistry.get("Non-existent task"), "Non-existent task should not be created");
